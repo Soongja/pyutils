5e323ee9a4b7ec68e3371ed1ba3fcd316c841622
Soongja <david0801@naver.com>
2018-10-16 14:58:45 +0900

    add pillow pixel color change

---
+++ PIL_pixel_change.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open('mask.png')
+pixels = img.load()
+
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        if pixels[i, j] == (255, 0, 0):
+            pixels[i, j] = (255, 255, 255)
+        else:
+            pixels[i, j] = (0, 0, 0)
+
+img = img.resize((224, 224))
+img = img.convert('L')
+img.save('mask_converted.png')
